32e5f4213a9dee288c17fb6fd1270e18c9f1dc1d
MatheusGrijo <matheusgrijo@matheusgrijo.com.br>
2018-08-18 12:57:44 -0300

    New version 0.0.0.2

--- Program.cs
+++ Program.cs
@@ -16,74 +16,113 @@ namespace BitBotBackToTheFuture
 
 
         //REAL NET
+        static string version = "0.0.0.2";
+        static string location = @"c:\bitmex\";
         static string bitmexKey = "";
         static string bitmexSecret = "";
+        static string timeGraph = "";
+        static string statusShort = "";
+        static string statusLong = "";
+        static string pair = "";
         static int qtdyContacts = 0;
+        static int interval = 0;
+        static int intervalOrder = 0;
         static double profit = 0;
-        static double fee = 0.075;
-        private static string bitmexDomain = "https://www.bitmex.com";
+        static double fee = 0;
+        private static string bitmexDomain = "";
         static BitMEX.BitMEXApi bitMEXApi = null;
+
+        
+
+
+
         public static void Main(string[] args)
         {
-            //Config
-            String key = System.IO.File.ReadAllText(@"c:\bitmex\key.txt");
-            bitmexKey = key.Split(';')[0].Trim();
-            bitmexSecret = key.Split(';')[1].Trim();
-            bitmexDomain = key.Split(';')[2].Trim();
-            qtdyContacts = int.Parse(key.Split(';')[3].Trim());
-            profit = double.Parse(key.Split(';')[4].Trim());
-
-            Console.WriteLine("Deleron - Back to the future - v0.0.0.2 - Bitmex version");
-            bitMEXApi = new BitMEX.BitMEXApi(bitmexKey, bitmexSecret, bitmexDomain);
-
-            Console.WriteLine("Total open orders: " + bitMEXApi.GetOpenOrders("XBTUSD").Count);
-            Console.WriteLine("");
-
-            //makeOrder("Sell");
-            while (true)
+            try
             {
-                List<BitMEX.Candle> lstCandle = bitMEXApi.GetCandleHistory("XBTUSD", 100, "1m");
+                //Config
+                log("Deleron - Back to the future - v"+version+" - Bitmex version");
+                log("by Matheus Grijo");
+                log("GITHUB http://github.com/matheusgrijo");
+                log(" ******* DONATE ********* ");
+                log("BTC 39DWjHHGXJh9q82ZrxkA8fiZoE37wL8jgh");
+                log("BCH qqzwkd4klrfafwvl7ru7p7wpyt5z3sjk6y909xq0qk");
+                log("ETH 0x3017E79f460023435ccD285Ff30Bd10834D20777");
+                log("ETC 0x088E7E67af94293DB55D61c7B55E2B098d2258D9");
+                log("Load config...");
 
-                double[] arrayPriceClose = new double[100];
-                int i = 0;
-                foreach (var candle in lstCandle)
+                String jsonConfig = System.IO.File.ReadAllText(@"c:\bitmex\key.txt");
+                JContainer jCointaner = (JContainer)JsonConvert.DeserializeObject(jsonConfig, (typeof(JContainer)));
+
+                bitmexKey = jCointaner["key"].ToString();
+                bitmexSecret = jCointaner["secret"].ToString();
+                bitmexDomain = jCointaner["domain"].ToString();
+                statusShort = jCointaner["short"].ToString();
+                statusLong = jCointaner["long"].ToString();
+                pair = jCointaner["pair"].ToString();
+                timeGraph = jCointaner["timeGraph"].ToString();
+                qtdyContacts = int.Parse(jCointaner["contract"].ToString());
+                interval = int.Parse(jCointaner["interval"].ToString());
+                intervalOrder = int.Parse(jCointaner["intervalOrder"].ToString());
+                profit = double.Parse(jCointaner["profit"].ToString());
+                fee = double.Parse(jCointaner["fee"].ToString());                
+                bitMEXApi = new BitMEX.BitMEXApi(bitmexKey, bitmexSecret, bitmexDomain);
+
+                log("OK!");
+
+                log("Total open orders: " + bitMEXApi.GetOpenOrders(pair).Count);
+                log("");
+
+                //makeOrder("Sell");
+                while (true)
                 {
-                    arrayPriceClose[i] = (double)candle.Close;
-                    i++;
+                    List<BitMEX.Candle> lstCandle = bitMEXApi.GetCandleHistory(pair, 100, timeGraph);
+
+                    double[] arrayPriceClose = new double[100];
+                    int i = 0;
+                    foreach (var candle in lstCandle)
+                    {
+                        arrayPriceClose[i] = (double)candle.Close;
+                        i++;
+                    }
+
+                    Array.Reverse(arrayPriceClose);
+
+                    double[] rsi = Indicators.RSI(arrayPriceClose, getIndicators("rsi","period"));
+                    double[] ema3 = Indicators.EMA(arrayPriceClose, getIndicators("emaShort","period"));
+                    double[] ema5 = Indicators.EMA(arrayPriceClose, getIndicators("emaLong","period"));
+                    //double[] cog = Indicators.COG(arrayPriceClose, 10);
+
+                    Console.Title = DateTime.Now.ToString() + " - " + pair + "(" + lstCandle[0].Close + ")";
+                    log(DateTime.Now.ToString());
+                    log("CLOSE PRICE " + lstCandle[0].Close);
+                    log("RSI " + rsi[99]);
+                    log("EMA LONG " + ema5[99]);
+                    log("EMA SHORT " + ema3[99]);
+                    //log("COG " + cog[99]);
+                    log("");
+
+                    //Strategy cross EMA and RSI
+                    if (ema3[98] < ema5[98] && ema3[99] > ema5[99] && rsi[99] > getIndicators("rsi", "limit") && rsi[98] > rsi[97])
+                    {
+                        makeOrder("Buy");
+                    }
+                    if (ema3[98] > ema5[98] && ema3[99] < ema5[99] && rsi[99] < getIndicators("rsi", "limit") && rsi[98] < rsi[97])
+                    {
+                        makeOrder("Sell");
+                    }
+
+                    log("wait " + interval + "ms");
+                    Thread.Sleep(interval);
                 }
 
-                Array.Reverse(arrayPriceClose);
-
-                double[] rsi = Indicators.RSI(arrayPriceClose, 7);
-                double[] ema3 = Indicators.EMA(arrayPriceClose, 3);
-                double[] ema5 = Indicators.EMA(arrayPriceClose, 5);
-                double[] cog = Indicators.COG(arrayPriceClose, 10);
-
-                Console.WriteLine(DateTime.Now);
-                Console.WriteLine("CLOSE PRICE " + lstCandle[0].Close);
-                Console.WriteLine("RSI " + rsi[99]);
-                Console.WriteLine("EMA 5 " + ema5[99]);
-                Console.WriteLine("EMA 3 " + ema3[99]);
-                Console.WriteLine("COG " + cog[99]);
-                Console.WriteLine("");
 
 
-                if (ema3[98] < ema5[98] && ema3[99] > ema5[99] && rsi[99] > 40 && rsi[98] > rsi[97])
-                {
-                    makeOrder("Buy");
-                }
-                if (ema3[98] > ema5[98] && ema3[99] < ema5[99] && rsi[99] < 40 && rsi[98] < rsi[97])
-                {
-                    makeOrder("Sell");
-                }
-
-                Console.WriteLine("wait 10s");
-                Thread.Sleep(10000);
             }
-
-
-
-
+            catch(Exception ex)
+            {
+                log("ERROR FATAL::" + ex.Message + ex.StackTrace);
+            }
 
 
         }
@@ -92,33 +131,62 @@ namespace BitBotBackToTheFuture
         static void makeOrder(string side)
         {
 
-            Console.WriteLine("Make order " + side);
-            if (side == "Sell")
+            log("Make order " + side);
+            if (side == "Sell" && statusShort == "enable")
             {
-                String json = bitMEXApi.MarketOrder("XBTUSD", "Sell", qtdyContacts);
-                Console.WriteLine(json);
+                String json = bitMEXApi.MarketOrder(pair, "Sell", qtdyContacts);
+                log(json);
                 JContainer jCointaner = (JContainer)JsonConvert.DeserializeObject(json, (typeof(JContainer)));
                 double price = double.Parse(jCointaner["price"].ToString().Replace(".", "."));
                 price -= (price * (fee + profit)) / 100;
                 price = Math.Floor(price);
-                json = bitMEXApi.PostOrderPostOnly("XBTUSD", "Buy", price, qtdyContacts);
-                Console.WriteLine(json);
+                json = bitMEXApi.PostOrderPostOnly(pair, "Buy", price, qtdyContacts);
+                log(json);
             }
-            if (side == "Buy")
+            if (side == "Buy" && statusLong == "enable")
             {
-                String json = bitMEXApi.MarketOrder("XBTUSD", "Buy", qtdyContacts);
-                Console.WriteLine(json);
+                String json = bitMEXApi.MarketOrder(pair, "Buy", qtdyContacts);
+                log(json);
                 JContainer jCointaner = (JContainer)JsonConvert.DeserializeObject(json, (typeof(JContainer)));
                 double price = double.Parse(jCointaner["price"].ToString().Replace(".", "."));
                 price += (price * (fee + profit)) / 100;
                 price = Math.Ceiling(price);
-                json = bitMEXApi.PostOrderPostOnly("XBTUSD", "Sell", price, qtdyContacts);
-                Console.WriteLine(json);
+                json = bitMEXApi.PostOrderPostOnly(pair, "Sell", price, qtdyContacts);
+                log(json);
             }
 
-            Console.WriteLine("wait 60s");
-            Thread.Sleep(60000);
+            log("wait " + intervalOrder + "ms");
+            Thread.Sleep(intervalOrder);
 
+        }
+
+
+        static int getIndicators(string nameIndicator,string field)
+        {
+            String jsonConfig = System.IO.File.ReadAllText(@"c:\bitmex\key.txt");
+            JContainer jCointaner = (JContainer)JsonConvert.DeserializeObject(jsonConfig, (typeof(JContainer)));
+            foreach (var item in jCointaner["indicators"])
+            {
+                if (item["name"].ToString() == nameIndicator)
+                {
+                    return int.Parse(item[field].ToString());
+                }
+            }
+            return 0;
+        }
+
+        static void log(string value)
+        {
+            try
+            {
+                value = "[" + DateTime.Now.ToString() + "] - " + value;
+                System.IO.StreamWriter w = new StreamWriter(location + DateTime.Now.ToString("yyyyMMdd") + "_log.txt",true);
+                w.WriteLine(value);
+                Console.WriteLine(value);
+                w.Close();
+                w.Dispose();
+            }
+            catch { }
         }
 
         class Indicators
